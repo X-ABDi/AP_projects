4c5d71653d7e12e283196c25f12421fb4b70dc3f
X-ABDi <fatemeabdi.sk@gmail.com>
2024-08-09 02:15:16 +0330

    add func for read input

--- HW0/train_tickets.cpp
+++ HW0/train_tickets.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -19,34 +20,75 @@ struct request {
     string traveller_count;
 } ;
 
-void getInput(vector<train> &trains, vector<request> &requests);
+void getInput(vector<train*> &trains, vector<request*> &requests);
+// void processInput(vector<train*> &trains, vector<request*> &requests,vector<train*> &available_trains);
+// void monitorOutput(vector<train*> &available_trains);
 
 int main()
 {
-    vector<train> trains;
-    vector<request> requests; 
+    vector<train*> trains;
+    vector<request*> requests; 
+    vector<train*> available_trains;
+    cout << "before getinput" << endl;
     getInput(trains , requests);
+
+    // processInput(trains, requests, available_trains);
+    // monitorOutput(available_trains);
+    
 
     return 0;
 }
 
-void filling_trains (int trains_num ,vector<train> &trains) {
+void fillingTrains (int trains_num ,vector<train*> &trains) {
     int i{0};
     string line;
-    
+    stringstream in_line;
+    cout << "inside filling train" << endl;
     for(i=0 ; i<trains_num ; i++) {
         getline (cin , line);
+        in_line << line;
         train* T = new train;
-
+        in_line >> T->name >> T->source >> T->destination >> T->time >> T->remaining_capacity;
+        trains.push_back(T);
+    }
+    cout << "it should print the vector of trains!!" << endl;
+    for(i=0 ; i<trains_num ; i++) {
+        cout << trains[i]->name << endl;
     }
 }
 
-void getInput(vector<train> &trains, vector<request> &requests)
+void fillingRequests (int requests_num, vector<request*> &requests){
+    int i{0};
+    string line;
+    stringstream in_line;
+    for(i=0 ; i<requests_num ; i++) {
+        getline (cin , line);
+        in_line << line;
+        request* R = new request;
+        in_line >> R->source >> R->destination >> R->first_time_possible >> R->traveller_count;
+        requests.push_back(R);
+    }
+
+    for(i=0 ; i<requests_num ; i++) {
+        cout << requests[i]->source << endl;
+    }
+}
+
+void getInput(vector<train*> &trains, vector<request*> &requests)
 {
     int trains_num{0};
     int requests_num{0};
-
+    cout << "inside getinput" << endl;
     cin >> trains_num >> requests_num;
-
-    filling_trains (trains_num , trains);
+    cout << "before filling stuff" << endl;
+    fillingTrains (trains_num , trains);
+    fillingRequests (requests_num, requests);
 }
+
+// void processInput(vector<train> &trains, vector<request> &requests,vector<train> &available_trains) {
+
+// }
+
+// void monitorOutput(vector<train> &available_trains){
+ 
+// }
