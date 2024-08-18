9a6b71c3ae911d2f7eead6f2798d65216c3bf230
X-ABDi <fatemeabdi.sk@gmail.com>
2024-08-18 22:54:57 +0330

    getting input from file

--- HW1/entekhab_vahed.cpp
+++ HW1/entekhab_vahed.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
 
+using namespace std;
+
+struct course 
+{
+    string course_name;
+    string course_unit;
+};
+
+struct presented_course : course
+{
+    vector<string> prerequisites;
+};
+
+struct taken_course : course
+{
+    string grade;
+};
+
+int main()
+{
+    
+
+}
